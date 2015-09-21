22643f8d0f4b3947b54e9dd0f06a64a2818f3574
CCpt Caveman <drummin_in_da_spirit@hotmail.com>
2015-09-21 15:16:27 -0600

    Initial code

---
+++ Craps.cpp
@@ -0,0 +1,15 @@
+
+
+
+int main( ) {
+  char ans;
+  bool done = false;
+  while ( ! done ) {
+         playGame();   // YOU MUST WRITE THIS FUNCTION
+         cout << " Play again (y/n) ? ";
+         cin >> ans;
+         if ( ans == 'y' || ans == 'Y') done = true;
+             else done = false;
+  }
+  return 0;
+}
